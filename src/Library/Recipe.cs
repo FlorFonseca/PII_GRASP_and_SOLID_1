9fabc99649053cc3f54e24cb9883f85a71e6c297
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2023-09-10 12:11:34 -0300

    Grasp solid 1 terminado

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,12 +6,14 @@
 
 using System;
 using System.Collections;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
 
 namespace Full_GRASP_And_SOLID.Library
 {
     public class Recipe
     {
-        private ArrayList steps = new ArrayList();
+        public ArrayList steps = new ArrayList();
 
         public Product FinalProduct { get; set; }
 
@@ -24,15 +26,28 @@ namespace Full_GRASP_And_SOLID.Library
         {
             this.steps.Remove(step);
         }
-
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
             {
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+                    $"usando '{step.Equipment.Description}' durante {step.Time} minutos");
             }
         }
+        public void GetProductionCost()
+        {
+            double SumaCostosIng=0;
+            double SumaCostosEquip=0;
+            
+            foreach (Step step in this.steps)
+            {
+                SumaCostosIng=+ step.Input.UnitCost*step.Quantity;
+                SumaCostosEquip=+ (step.Time/60)*step.Equipment.HourlyCost;
+            }
+            double CostoTotal=SumaCostosEquip+SumaCostosIng;
+            Console.WriteLine($"El costo total de la receta es: {CostoTotal}");
+        }
+
     }
 }
